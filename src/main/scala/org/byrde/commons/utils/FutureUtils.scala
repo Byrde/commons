85dfda52bf8c33f5cfb9c7a6d9e15d86bd07593e
Martin Allaire <martin.allaire@razorfish.com>
2018-05-03 13:56:15 -0400

    Adding execution context

--- src/main/scala/org/byrde/commons/utils/FutureUtils.scala
+++ src/main/scala/org/byrde/commons/utils/FutureUtils.scala
@@ -1,11 +1,11 @@
 package org.byrde.commons.utils
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.implicitConversions
 import scala.util.{Failure, Success, Try}
 
 object FutureUtils {
-  implicit def FutureTryToFuture[T](value: Future[Try[T]]): Future[T] =
+  implicit def FutureTryToFuture[T](value: Future[Try[T]])(implicit ec: ExecutionContext): Future[T] =
     value.flatMap {
       case Success(res) =>
         Future.successful(res)
