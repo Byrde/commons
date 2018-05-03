82342da8d009b4648518c1af13639e265a9d0f13
Martin Allaire <martin.allaire@razorfish.com>
2018-05-03 14:22:32 -0400

    Adding flattenTry

--- src/main/scala/org/byrde/commons/utils/FutureUtils.scala
+++ src/main/scala/org/byrde/commons/utils/FutureUtils.scala
@@ -5,11 +5,21 @@ import scala.language.implicitConversions
 import scala.util.{Failure, Success, Try}
 
 object FutureUtils {
-  implicit def FutureTryToFuture[T](value: Future[Try[T]])(implicit ec: ExecutionContext): Future[T] =
+  implicit def FutureTry2FutureConversion[T](value: Future[Try[T]])(implicit ec: ExecutionContext): Future[T] =
     value.flatMap {
       case Success(res) =>
         Future.successful(res)
       case Failure(ex) =>
         Future.failed(ex)
     }
+
+  implicit class FutureTry2Future[T](value: Future[Try[T]])(implicit ec: ExecutionContext) {
+    def flattenTry: Future[T] =
+      value.flatMap {
+        case Success(res) =>
+          Future.successful(res)
+        case Failure(ex) =>
+          Future.failed(ex)
+      }
+  }
 }
