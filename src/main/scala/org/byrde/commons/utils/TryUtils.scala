67e5646183df4fed37aab7cb358f508f0d620486
Martin Allaire <martin.allaire@razorfish.com>
2018-05-03 11:38:22 -0400

    Supposed to have proper type parameter for Try

--- src/main/scala/org/byrde/commons/utils/TryUtils.scala
+++ src/main/scala/org/byrde/commons/utils/TryUtils.scala
@@ -8,8 +8,8 @@ object TryUtils {
     @inline def + : Try[T] = Success(value)
   }
 
-  implicit class Throwable2Failure(value: Throwable) {
-    @inline def failure: Try[Throwable] = Failure(value)
-    @inline def - : Try[Throwable] = Failure(value)
+  implicit class Throwable2Failure[T](value: Throwable) {
+    @inline def failure: Try[T] = Failure(value)
+    @inline def - : Failure[T] = Failure(value)
   }
 }
