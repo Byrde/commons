0d363e43ee287134bc354cbe01ed758e2cf5cfde
Martin Allaire <martin.allaire@razorfish.com>
2018-05-03 11:34:11 -0400

    Adding TryUtils

---
+++ src/main/scala/org/byrde/commons/utils/TryUtils.scala
@@ -0,0 +1,15 @@
+package org.byrde.commons.utils
+
+import scala.util.{Failure, Success, Try}
+
+class TryUtils {
+  implicit class Any2Success[T](value: T) {
+    @inline def success: Try[T] = Success(value)
+    @inline def + : Try[T] = Success(value)
+  }
+
+  implicit class Throwable2Failure(value: Throwable) {
+    @inline def failure: Try[Throwable] = Failure(value)
+    @inline def - : Try[Throwable] = Failure(value)
+  }
+}
