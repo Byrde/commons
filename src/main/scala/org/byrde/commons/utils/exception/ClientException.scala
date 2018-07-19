026ea40f30e4893c5bf6509fb18b2ebbb12e863f
John Doe <mallaire77@gmail.com>
2018-07-18 20:02:09 -0400

    import NoStackTrace

--- src/main/scala/org/byrde/commons/utils/exception/ClientException.scala
+++ src/main/scala/org/byrde/commons/utils/exception/ClientException.scala
@@ -1,5 +1,7 @@
 package org.byrde.commons.utils.exception
 
+import scala.util.control.NoStackTrace
+
 case class ClientException(_msg: String, _code: Int, _status: Int) extends ServiceResponseException[ClientException](_msg, _code, _status) with NoStackTrace {
   override def apply(message: String): ClientException =
     ClientException(message, _code, _status)
