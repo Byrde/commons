a73f1f150ef7d5c72f74c8a6f910c211ec40208d
Martin <mallaire@walmartlabs.com>
2018-07-24 13:46:24 -0400

    Better handling for json service requests

--- src/main/scala/org/byrde/commons/utils/exception/ServiceResponseException.scala
+++ src/main/scala/org/byrde/commons/utils/exception/ServiceResponseException.scala
@@ -2,6 +2,8 @@ package org.byrde.commons.utils.exception
 
 import org.byrde.commons.models.services.DefaultServiceResponse
 
+import scala.util.control.NoStackTrace
+
 abstract class ServiceResponseException[T <: ServiceResponseException[T]](_msg: String, _code: Int, _status: Int)
     extends Throwable(_msg)
     with DefaultServiceResponse {
@@ -17,4 +19,11 @@ abstract class ServiceResponseException[T <: ServiceResponseException[T]](_msg: 
 
   override def code: Int =
     _code
+}
+
+object ServiceResponseException {
+  case class TransientServiceResponseException(_msg: String, _code: Int, _status: Int) extends ServiceResponseException[TransientServiceResponseException](_msg, _code, _status) with NoStackTrace {
+    override def apply(throwable: Throwable): TransientServiceResponseException =
+      TransientServiceResponseException(throwable.getMessage, code, status)
+  }
 }
