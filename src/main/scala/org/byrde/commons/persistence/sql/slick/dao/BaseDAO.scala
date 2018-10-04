d89e7f7afca27e0fe902ca7883954512861b6fbb
John Doe <mallaire77@gmail.com>
2018-10-04 13:07:22 -0400

    Removing db reference from dao

--- src/main/scala/org/byrde/commons/persistence/sql/slick/dao/BaseDAO.scala
+++ src/main/scala/org/byrde/commons/persistence/sql/slick/dao/BaseDAO.scala
@@ -1,29 +1,17 @@
 package org.byrde.commons.persistence.sql.slick.dao
 
+import org.byrde.commons.persistence.sql.slick.Role
 import org.byrde.commons.persistence.sql.slick.sqlbase.BaseEntity
 import org.byrde.commons.persistence.sql.slick.sqlbase.conf.Profile
-import org.byrde.commons.persistence.sql.slick.sqlbase.db.Db
 import org.byrde.commons.persistence.sql.slick.sqlbase.table.Tables
-import org.byrde.commons.persistence.sql.slick.{HasPrivilege, Role}
 
 import slick.lifted.CanBeQueryCondition
 import slick.sql.{FixedSqlAction, FixedSqlStreamingAction, SqlAction}
 
-import scala.concurrent.Future
-import scala.language.higherKinds
-
-abstract class BaseDAO[R <: Role, Entity <: BaseEntity, TableType <: Tables#BaseTable[Entity]](val profile: Profile[R]) {
+abstract class BaseDAO[R <: Role, Entity <: BaseEntity, TableType <: Tables#BaseTable[Entity]](protected val profile: Profile[R]) {
   import profile.api._
 
   def QueryBuilder: TableQuery[TableType]
-
-  implicit class Query2Run[T, E <: Effect](query: DBIOAction[T, NoStream, E]) {
-    def run(implicit ev: R HasPrivilege E): Future[T] =
-      db.run(query)
-  }
-
-  protected val db =
-    Db(profile)
 
   protected def findById(id: Long): SqlAction[Option[Entity], NoStream, Effect.Read] =
     QueryBuilder.withFilter(_.id === id).result.headOption
