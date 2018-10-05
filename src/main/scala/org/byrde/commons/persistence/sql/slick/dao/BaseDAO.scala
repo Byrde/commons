4987101f07220d2b354e38d00160cab23d3775d5
John Doe <mallaire77@gmail.com>
2018-10-05 15:11:36 -0400

    Removing protected

--- src/main/scala/org/byrde/commons/persistence/sql/slick/dao/BaseDAO.scala
+++ src/main/scala/org/byrde/commons/persistence/sql/slick/dao/BaseDAO.scala
@@ -12,22 +12,22 @@ abstract class BaseDAO[Entity <: BaseEntity, TableType <: BaseTables#BaseTable[E
 
   def QueryBuilder: TableQuery[TableType]
 
-  protected def findById(id: Long): SqlAction[Option[Entity], NoStream, Effect.Read] =
+  def findById(id: Long): SqlAction[Option[Entity], NoStream, Effect.Read] =
     QueryBuilder.withFilter(_.id === id).result.headOption
 
-  protected def findByFilter[QueryCondition: CanBeQueryCondition](f: TableType => QueryCondition): FixedSqlStreamingAction[Seq[Entity], Entity, Effect.Read] =
+  def findByFilter[QueryCondition: CanBeQueryCondition](f: TableType => QueryCondition): FixedSqlStreamingAction[Seq[Entity], Entity, Effect.Read] =
     QueryBuilder.withFilter(f).result
 
-  protected def inserts(rows: Entity*): FixedSqlAction[Seq[Long], NoStream, Effect.Write] =
+  def inserts(rows: Entity*): FixedSqlAction[Seq[Long], NoStream, Effect.Write] =
     QueryBuilder returning QueryBuilder.map(_.id) ++= rows
 
-  protected def deleteByIds(ids: Long*): FixedSqlAction[Int, NoStream, Effect.Write] =
+  def deleteByIds(ids: Long*): FixedSqlAction[Int, NoStream, Effect.Write] =
     QueryBuilder.withFilter(_.id.inSet(ids)).delete
 
-  protected def deleteByFilter[QueryCondition : CanBeQueryCondition](f: TableType => QueryCondition): FixedSqlAction[Int, NoStream, Effect.Write] =
+  def deleteByFilter[QueryCondition : CanBeQueryCondition](f: TableType => QueryCondition): FixedSqlAction[Int, NoStream, Effect.Write] =
     QueryBuilder.withFilter(f).delete
 
-  protected def update[Value: ColumnType, QueryCondition: CanBeQueryCondition](f: TableType => QueryCondition)(value: Value, field: TableType => Rep[Value]): FixedSqlAction[Int, NoStream, Effect.Write] = {
+  def update[Value: ColumnType, QueryCondition: CanBeQueryCondition](f: TableType => QueryCondition)(value: Value, field: TableType => Rep[Value]): FixedSqlAction[Int, NoStream, Effect.Write] = {
     val query =
       for {
         c <- QueryBuilder.withFilter(f)
