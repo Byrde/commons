30b90e222b3d11a4a6e4f0feb2333e3597e56dd9
John Doe <mallaire77@gmail.com>
2018-10-09 18:45:21 -0400

    Reworked Repositories

---
+++ src/main/scala/org/byrde/commons/persistence/sql/slick/repository/Repository.scala
@@ -0,0 +1,17 @@
+package org.byrde.commons.persistence.sql.slick.repository
+
+import org.byrde.commons.persistence.sql.slick.conf.Profile
+import org.byrde.commons.persistence.sql.slick.db.Db
+import org.byrde.commons.persistence.sql.slick.{HasPrivilege, Role}
+
+import scala.concurrent.Future
+
+trait Repository[R <: Role] {
+  self: Profile[R] with Db[R] =>
+    import profile.api._
+
+    implicit class Query2Runnable[Result, E <: Effect](query: DBIOAction[Result, NoStream, E]) {
+      def run(implicit ev: R HasPrivilege E): Future[Result] =
+        self.run(query)
+    }
+}
