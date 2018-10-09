c839c7cc852d94d238b002641f3cc6e07b999659
John Doe <mallaire77@gmail.com>
2018-10-09 13:16:17 -0400

    Renaming

--- src/main/scala/org/byrde/commons/persistence/sql/slick/repository/Repositories.scala
+++ src/main/scala/org/byrde/commons/persistence/sql/slick/repository/Repositories.scala
@@ -2,13 +2,13 @@ package org.byrde.commons.persistence.sql.slick.repository
 
 import org.byrde.commons.persistence.sql.slick.{HasPrivilege, Role}
 import org.byrde.commons.persistence.sql.slick.db.Db
-import org.byrde.commons.persistence.sql.slick.repository.BaseRepositories.RunnableQuery
+import org.byrde.commons.persistence.sql.slick.repository.Repositories.RunnableQuery
 
 import slick.dbio.{DBIOAction, Effect, NoStream}
 
 import scala.concurrent.Future
 
-trait BaseRepositories[R <: Role] {
+trait Repositories[R <: Role] {
   self: Db[R] =>
     def query[Result, E <: Effect](query: self.type => DBIOAction[Result, NoStream, E])(implicit ev: R HasPrivilege E): RunnableQuery[Result] =
       new RunnableQuery[Result] {
@@ -17,7 +17,7 @@ trait BaseRepositories[R <: Role] {
       }
 }
 
-object BaseRepositories {
+object Repositories {
   trait RunnableQuery[Result] {
     def run: Future[Result]
   }
