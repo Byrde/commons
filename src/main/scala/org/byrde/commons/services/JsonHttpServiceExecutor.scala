40145a525f031bc309085ce4196d410c9f73cfff
Martin Allaire <mallaire77@gmail.com>
2018-10-12 15:15:56 -0400

    Fixed executor

--- src/main/scala/org/byrde/commons/services/JsonHttpServiceExecutor.scala
+++ src/main/scala/org/byrde/commons/services/JsonHttpServiceExecutor.scala
@@ -22,26 +22,26 @@ abstract class JsonHttpServiceExecutor extends HttpServiceExecutor {
 
   def circuitBreaker: CircuitBreakerLike
 
-  def get[T: ClassTag](path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => T] = None)(implicit reads: Reads[T]): Future[T] =
+  def get[T: ClassTag](path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => ServiceResponse[T]] = None)(implicit format: Format[T]): Future[ServiceResponse[T]] =
     super.underlyingGet(path, requestHook, curlRequestHook).map(processResponse[T](_, errorHook))(ec)
 
-  def post[T, TT: ClassTag](body: T)(path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => TT] = None)(implicit bodyWritable: BodyWritable[T], reads: Reads[TT]): Future[TT] =
+  def post[T, TT: ClassTag](body: T)(path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => ServiceResponse[TT]] = None)(implicit bodyWritable: BodyWritable[T], format: Format[TT]): Future[ServiceResponse[TT]] =
     super.underlyingPost(body)(path, requestHook, curlRequestHook).map(processResponse[TT](_, errorHook))(ec)
 
-  def put[T, TT: ClassTag](body: T)(path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => TT] = None)(implicit bodyWritable: BodyWritable[T], reads: Reads[TT]): Future[TT] =
+  def put[T, TT: ClassTag](body: T)(path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => ServiceResponse[TT]] = None)(implicit bodyWritable: BodyWritable[T], format: Format[TT]): Future[ServiceResponse[TT]] =
     super.underlyingPut(body)(path, requestHook, curlRequestHook).map(processResponse[TT](_, errorHook))(ec)
 
-  def delete[T: ClassTag](path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => T] = None)(implicit reads: Reads[T]): Future[T] =
+  def delete[T: ClassTag](path: Path, requestHook: StandaloneWSRequest => StandaloneWSRequest = identity, curlRequestHook: CurlRequest => Unit = _ => (), errorHook: Option[JsParsingError => ServiceResponse[T]] = None)(implicit format: Format[T]): Future[ServiceResponse[T]] =
     super.underlyingDelete(path, requestHook, curlRequestHook).map(processResponse[T](_, errorHook))(ec)
 
   override def executeRequest(request: StandaloneWSRequest): Future[StandaloneWSResponse] =
     circuitBreaker.withCircuitBreaker(request.execute().map(identity)(ec))
 
-  private def processResponse[T: ClassTag](response: StandaloneWSResponse, errorHook: Option[JsParsingError => T])(implicit reads: Reads[T]): T =
+  private def processResponse[T: ClassTag](response: StandaloneWSResponse, errorHook: Option[JsParsingError => ServiceResponse[T]])(implicit format: Format[T]): ServiceResponse[T] =
     Json
       .parse(response.body)
       .errorHook
-      .validate[T] match {
+      .validate[TransientServiceResponse[T]](ServiceResponse.reads(format)) match {
         case JsSuccess(validated, _) =>
           validated
 
