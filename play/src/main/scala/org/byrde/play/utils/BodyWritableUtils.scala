a3978b9cbcc3db410b426c695d9d96f381ab237d
Martin <martin@byrde.io>
2019-03-11 16:13:27 -0400

    Adding writeable for any content

--- play/src/main/scala/org/byrde/play/utils/BodyWritableUtils.scala
+++ play/src/main/scala/org/byrde/play/utils/BodyWritableUtils.scala
@@ -1,8 +1,12 @@
 package org.byrde.play.utils
 
 import play.api.libs.ws.{BodyWritable, EmptyBody}
+import play.api.mvc.AnyContent
 
 object BodyWritableUtils {
   implicit val writeableOf_EmptyBody: BodyWritable[Unit] =
+    new BodyWritable(_ => EmptyBody, "text/plain")
+
+  implicit val writeableOf_AnyContent: BodyWritable[AnyContent] =
     new BodyWritable(_ => EmptyBody, "text/plain")
 }
