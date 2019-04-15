0792aca84462d8c0dea890d3bd800b5e8343f075
Martin <martin@byrde.io>
2019-04-14 20:18:08 -0400

    With no stack trace

--- clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedServiceResponseException.scala
+++ clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedServiceResponseException.scala
@@ -1,4 +1,6 @@
 package org.byrde.clients.ahc
 
+import scala.util.control.NoStackTrace
+
 case class BoxedServiceResponseException(protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Exception)
-  extends Exception
+  extends Exception with NoStackTrace
--- clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedTypedServiceResponseException.scala
+++ clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedTypedServiceResponseException.scala
@@ -1,8 +1,9 @@
 package org.byrde.clients.ahc
 
 import scala.reflect.ClassTag
+import scala.util.control.NoStackTrace
 
 case class BoxedTypedServiceResponseException[T: ClassTag](protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Throwable)
-  extends Exception {
+  extends Exception with NoStackTrace {
   type TypeSafeResponse = T
 }
