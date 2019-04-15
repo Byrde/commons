4c06c25cff982c52224924ceb11d5251c8ce3738
Martin <martin@byrde.io>
2019-04-14 20:16:54 -0400

    Supposed to be val

--- clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedServiceResponseException.scala
+++ clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedServiceResponseException.scala
@@ -1,4 +1,4 @@
 package org.byrde.clients.ahc
 
-case class BoxedServiceResponseException(protocol: String, host: String, port: Option[String], method: String, path: String)(exception: Exception)
+case class BoxedServiceResponseException(protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Exception)
   extends Exception
--- clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedTypedServiceResponseException.scala
+++ clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedTypedServiceResponseException.scala
@@ -2,7 +2,7 @@ package org.byrde.clients.ahc
 
 import scala.reflect.ClassTag
 
-case class BoxedTypedServiceResponseException[T: ClassTag](protocol: String, host: String, port: Option[String], method: String, path: String)(exception: Throwable)
+case class BoxedTypedServiceResponseException[T: ClassTag](protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Throwable)
   extends Exception {
   type TypeSafeResponse = T
 }
