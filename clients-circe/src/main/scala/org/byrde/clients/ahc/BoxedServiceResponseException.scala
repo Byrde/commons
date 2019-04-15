9053a70e01457ffe89f8f80b99968bcd49544092
Martin <martin@byrde.io>
2019-04-14 21:17:42 -0400

    Changing to throwable

--- clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedServiceResponseException.scala
+++ clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedServiceResponseException.scala
@@ -2,5 +2,5 @@ package org.byrde.clients.ahc
 
 import scala.util.control.NoStackTrace
 
-case class BoxedServiceResponseException(protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Exception)
-  extends Exception with NoStackTrace
+case class BoxedServiceResponseException(protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Throwable)
+  extends Exception(exception.getMessage) with NoStackTrace
--- clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedTypedServiceResponseException.scala
+++ clients-circe/src/main/scala/org/byrde/clients/ahc/BoxedTypedServiceResponseException.scala
@@ -4,6 +4,6 @@ import scala.reflect.ClassTag
 import scala.util.control.NoStackTrace
 
 case class BoxedTypedServiceResponseException[T: ClassTag](protocol: String, host: String, port: Option[String], method: String, path: String)(val exception: Throwable)
-  extends Exception with NoStackTrace {
+  extends Exception(exception.getMessage) with NoStackTrace {
   type TypeSafeResponse = T
 }
