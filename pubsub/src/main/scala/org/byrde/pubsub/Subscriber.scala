162ca58ccd400edf219f17338257c6d905130458
Martin <martin@byrde.io>
2022-01-09 13:55:03 -0500

    Fixing subscriber

--- pubsub/src/main/scala/org/byrde/pubsub/Subscriber.scala
+++ pubsub/src/main/scala/org/byrde/pubsub/Subscriber.scala
@@ -2,7 +2,8 @@ package org.byrde.pubsub
 
 import com.google.api.gax.core.CredentialsProvider
 import com.google.auth.Credentials
-import com.google.cloud.pubsub.v1.MessageReceiver
+import com.google.cloud.pubsub.v1.{MessageReceiver, SubscriptionAdminClient}
+import com.google.protobuf.Duration
 
 import org.byrde.logging.Logger
 
@@ -18,25 +19,62 @@ import scala.util.chaining._
 trait Subscriber {
   def subscribe[T](
     credentials: Credentials,
-    subscription: String,
+    subscription: Subscription,
+    topic: Topic
   )(fn: Envelope[T] => Future[_])(implicit logger: Logger, decoder: Decoder[T]): Future[Unit] =
-    receiver(fn)
-      .pipe(com.google.cloud.pubsub.v1.Subscriber.newBuilder(subscription, _))
-      .setCredentialsProvider {
-        new CredentialsProvider {
-          override def getCredentials: Credentials = credentials
+    (for {
+      _ <-
+        Try {
+          logger.logInfo(s"Creating subscription: $subscription")
+          SubscriptionAdminClient
+            .create()
+            .createSubscription {
+              com.google.pubsub.v1.Subscription
+                .newBuilder()
+                .setName(subscription)
+                .setTopic(topic)
+                .setAckDeadlineSeconds(10)
+                .setMessageRetentionDuration(Duration.newBuilder().setSeconds(604800).build())
+                .setExpirationPolicy(com.google.pubsub.v1.ExpirationPolicy.newBuilder().build())
+                .setRetryPolicy {
+                  com.google.pubsub.v1.RetryPolicy
+                    .newBuilder()
+                    .setMinimumBackoff(Duration.newBuilder().setSeconds(1).build())
+                    .setMaximumBackoff(Duration.newBuilder().setSeconds(180).build())
+                }
+                .build()
+            }
         }
-      }
-      .build()
-      .pipe { sub =>
+      subscriber <-
+        Try {
+          logger.logInfo(s"Creating subscriber: $subscription")
+          com.google.cloud.pubsub.v1.Subscriber
+            .newBuilder(subscription, receiver(fn))
+            .setCredentialsProvider {
+              new CredentialsProvider {
+                override def getCredentials: Credentials = credentials
+              }
+            }
+            .build()
+        }
+    } yield subscriber).pipe {
+      case Success(subscriber) =>
         logger.logInfo(s"Starting subscriber $subscription!")
-        Future(sub.awaitRunning())
-          .recoverWith {
-            case ex =>
-              logger.logError("Error starting subscriber!", ex)
-              Future(sub.awaitTerminated()).flatMap(_ => Future.failed(ex))
-          }
-      }
+        for {
+          _ <-
+            Future.unit
+          _ <-
+            Future(subscriber.awaitRunning())
+              .recoverWith {
+                case ex =>
+                  logger.logError("Error starting subscriber!", ex)
+                  Future(subscriber.awaitTerminated()).flatMap(_ => Future.failed(ex))
+              }
+        } yield ()
+
+      case Failure(exception) =>
+        Future.failed(exception)
+    }
   
   private def receiver[T](
     fn: Envelope[T] => Future[_],
@@ -45,7 +83,7 @@ trait Subscriber {
     parse(message.getData.toStringUtf8)
       .flatMap(_.as[Envelope[T]])
       .left
-      .map(PubSubError.DecodingError.apply(s"Error decoding message: ${message}")(_))
+      .map(PubSubError.DecodingError.apply(s"Error decoding message: $message")(_))
       .pipe {
         case Right(value) =>
           fn(value)
