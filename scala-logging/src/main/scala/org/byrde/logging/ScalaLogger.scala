9a891f07248b5f7943a3e72fb5a34d16396d30ff
Martin Allaire <martin@byrde.io>
2022-11-17 16:04:54 -0500

    Fixing logger

--- scala-logging/src/main/scala/org/byrde/logging/ScalaLogger.scala
+++ scala-logging/src/main/scala/org/byrde/logging/ScalaLogger.scala
@@ -1,6 +1,6 @@
 package org.byrde.logging
 
-import net.logstash.logback.marker.Markers._
+import net.logstash.logback.argument.StructuredArguments._
 
 import java.util.UUID
 
@@ -11,34 +11,34 @@ class ScalaLogger(name: String) extends Logger {
 
   override def logDebug(msg: String): Unit =
     logger.debug(msg)
-  
+
   override def logDebug(msg: String, extras: Log*): Unit =
-    logger.debug(msg, appendEntries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
+    logger.debug(msg, entries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
 
   override def logInfo(msg: String): Unit =
     logger.info(msg)
-  
+
   override def logInfo(msg: String, extras: Log*): Unit =
-    logger.info(msg, appendEntries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
+    logger.info(msg, entries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
 
   override def logWarning(msg: String): Unit =
     logger.warn(msg)
-  
+
   override def logWarning(msg: String, extras: Log*): Unit =
-    logger.warn(msg, appendEntries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
+    logger.warn(msg, entries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
 
   override def logError(msg: String): Unit =
     logger.error(msg)
-  
+
   override def logError(msg: String, extras: Log*): Unit =
-    logger.error(msg, appendEntries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
+    logger.error(msg, entries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
 
   override def logError(msg: String, cause: Throwable): Unit =
     logger.error(msg, cause)
 
   override def logError(msg: String, cause: Throwable, extras: Log*): Unit = {
     val id = UUID.randomUUID.toString.take(4)
-    logger.error(s"[$id] $msg", extras.foldLeft(Log.empty)(_ ++ _).asMap)
+    logger.error(s"[$id] $msg", entries(extras.foldLeft(Log.empty)(_ ++ _).asMap.asJava))
     logger.error(s"[$id] $msg", cause)
   }
 }
